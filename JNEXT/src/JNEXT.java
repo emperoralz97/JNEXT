a335ac7c7dcd988cd7aa5b62a5cc632bd6899871
emperoralz97 <emperorabbas@gmail.com>
2018-05-01 22:04:41 +0530

    added jnext.java

--- JNEXT/src/JNEXT.java
+++ JNEXT/src/JNEXT.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+import java.util.Stack;
 
 public class JNEXT {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		Stack<Integer> stk = new Stack<Integer>();
+		
 
 	}
 
